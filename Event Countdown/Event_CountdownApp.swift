1d01490e4d3ba6579525e8bca76634bba6ce2f4e
tnanhd <tnanh.dev@gmail.com>
2024-11-10 21:02:28 +0700

    Initial Commit

---
+++ Event Countdown/Event_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountdownApp.swift
+//  Event Countdown
+//
+//  Created by Tran Nhat Anh on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
